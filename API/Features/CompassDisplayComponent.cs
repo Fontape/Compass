c4261b938310ec33f15f1cde7f21b51ebe290535
No738 <no738.000@gmail.com>
2022-08-06 14:03:05 +1000

    moved FixedUpdate preconditions into method, fixed every frame updated bug, make several methods static

--- API/Features/CompassDisplayComponent.cs
+++ API/Features/CompassDisplayComponent.cs
@@ -16,12 +16,7 @@ namespace Compass.API.Features
 
         public void FixedUpdate()
         {
-            if (_compassViewer.IsDead ||
-                (CompassPlugin.CompassVisibilityMode == CompassVisibilityMode.FirearmHolders &&
-                 _compassViewer.CurrentItem?.IsWeapon == false) ||
-                (CompassPlugin.CompassVisibilityMode == CompassVisibilityMode.Squads &&
-                 _compassViewer.Role.Team != Team.MTF &&
-                 _compassViewer.Role.Team != Team.CHI))
+            if (IsCompassEnabled() == false)
             {
                 return;
             }
@@ -33,20 +28,43 @@ namespace Compass.API.Features
             }
 
             var viewerRotation = (int)_compassViewer.Rotation.y;
-            _compassViewer.Broadcast(1, $"{DrawCompassLine(viewerRotation)}",
-                Broadcast.BroadcastFlags.Monospaced, true);
+            _secondsToNextTick = 0;
+
+            _compassViewer.Broadcast(1,
+                $"{DrawCompassLine(viewerRotation)}",
+                Broadcast.BroadcastFlags.Monospaced,
+                true);
         }
 
         public void DestroySelf() => Destroy(this);
 
-        public int GetOverflowedDegrees(int degrees) => degrees switch
+        private static string DrawCompassLine(int degrees)
+        {
+            int leftDegrees = GetOverflowedDegrees(degrees - 45);
+            int rightDegrees = GetOverflowedDegrees(degrees + 45);
+
+            var lineBuilder = new StringBuilder();
+
+            for (var i = 0; i < 60; i++)
+            {
+                lineBuilder.Append("|");
+            }
+
+            var longSpaceBuilder = new string(' ', 10);
+
+            return $"<b><size=24>{GetCardinalDirection(degrees)}" +
+                   $"\n{lineBuilder}</size></b>" +
+                   $"\n<size=32><color=#ccc>{leftDegrees}°</color></size>{longSpaceBuilder}<b>{degrees}°</b>{longSpaceBuilder}<size=32><color=#ccc>{rightDegrees}°</color></size>";
+        }
+
+        public static int GetOverflowedDegrees(int degrees) => degrees switch
         {
             < 0 => 360 - degrees,
             > 360 => degrees - 360,
             _ => degrees
         };
 
-        private string GetCardinalDirection(int degrees) => degrees switch
+        private static string GetCardinalDirection(int degrees) => degrees switch
         {
             >= 0 and <= 90 => "СЕВЕР",
             > 90 and <= 180 => "ВОСТОК",
@@ -55,20 +73,18 @@ namespace Compass.API.Features
             _ => throw new InvalidOperationException()
         };
 
-        private string DrawCompassLine(int degrees)
+        private bool IsCompassEnabled()
         {
-            int leftDegrees = GetOverflowedDegrees(degrees - 45);
-            int rightDegrees = GetOverflowedDegrees(degrees + 45);
+            switch (CompassPlugin.CompassVisibilityMode)
+            {
+                case CompassVisibilityMode.FirearmHolders when _compassViewer.CurrentItem == null || _compassViewer.CurrentItem.IsWeapon == false:
+                case CompassVisibilityMode.SquadMembers when _compassViewer.Role.Team != Team.MTF && _compassViewer.Role.Team != Team.CHI:
+                    return false;
 
-            var lineBuilder = new StringBuilder();
-
-            for (var i = 0; i < 60; i++) lineBuilder.Append("|");
-
-            var longSpaceBuilder = new string(' ', 10);
-
-            return $"<b><size=24>{GetCardinalDirection(degrees)}" +
-                   $"\n{lineBuilder}</size></b>" +
-                   $"\n<size=32><color=#ccc>{leftDegrees}°</color></size>{longSpaceBuilder}<b>{degrees}°</b>{longSpaceBuilder}<size=32><color=#ccc>{rightDegrees}°</color></size>";
+                case CompassVisibilityMode.Humans:
+                default:
+                    return _compassViewer.IsHuman;
+            }
         }
     }
 }
